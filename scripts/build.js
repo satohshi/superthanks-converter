073a0476c73593973b28cd15a70688c1f1b5890e
Satoshi <140612116+satohshi@users.noreply.github.com>
2025-03-13 16:32:05 +0900

    remove _API_KEY as it's no longer used

--- scripts/build.js
+++ scripts/build.js
@@ -49,7 +49,6 @@ if (args.includes('--dev')) {
 		...BUILD_OPTIONS,
 		outfile: 'dev/index.js',
 		define: {
-			_API_KEY: JSON.stringify(process.env.API_KEY),
 			_LOCALE: JSON.stringify(process.env.LOCALE),
 			_CURRENCY: JSON.stringify(process.env.CURRENCY),
 		},
@@ -65,7 +64,6 @@ if (args.includes('--dev')) {
 			...BUILD_OPTIONS,
 			outfile: 'dist/index.js',
 			define: {
-				_API_KEY: '"YOUR_API_KEY"',
 				_LOCALE: '"YOUR_LOCALE"',
 				_CURRENCY: '"YOUR_CURRENCY"',
 			},
